7ef66b603efbdc93edb2ea010ae3d4852539cea5
wania akram <waniaakram777@gmail.com>
2024-12-10 01:22:08 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import Hero from "@/components/hero";
+import Brand from "@/components/brand";
+import Ceramics from "@/components/ceramics";
+import Product from "@/components/product";
+import Benefit from "@/components/benefit";
+import Touch from "@/components/touch";
+import ProductListing from "./productlisting/page";
+
+
+export default function Home() {
+  return (
+   <>
+   <Hero />
+   <Brand />
+   <Ceramics />
+   <Product />
+   <Benefit />
+   <Touch />
+<ProductListing/>
+   
+
+   </>
+  );
+}
